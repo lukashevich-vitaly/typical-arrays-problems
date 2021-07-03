418fea10e08f72291abda2b9b68a73a859fbd056
lukashevich-vitaly <lykashevich.vitalij@gmail.com>
2021-07-03 22:38:20 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+exports.min = function min(array) {
+    if (Array.isArray(array) && array.length > 0) {
+        return Math.min.apply(null, array);
+    } else {
+        return 0;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (Array.isArray(array) && array.length > 0) {
+        return Math.max.apply(null, array);
+    } else {
+        return 0;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    const c = (a, b) => a + b;
+    if (Array.isArray(array) && array.length > 0) {
+        return array.reduce(c) / array.length;
+    } else {
+        return 0;
+    }
+};
